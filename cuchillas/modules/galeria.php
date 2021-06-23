433fbbad5416e3fd95aae45a374ed6db09dbc091
Emilianomontani <63806214+Emilianomontani@users.noreply.github.com>
2021-06-23 02:39:12 +0200

    Add files via upload

---
+++ cuchillas/modules/galeria.php
@@ -0,0 +1,57 @@
+<section class="panel b-green" id="4">
+  <article class="panel__wrapper">
+    <div class="panel__content">
+      <h2 class="panel__headline"><i class="fa fa-paper-plane"></i>&nbsp;GALERÍA</h2>
+        <div class="panel__block"></div>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a href="views/images/back2.JPG" class="fancybox" rel="ligthbox">
+                  <img  src="views/images/back2.JPG" class="zoom img-fluid "  alt="">
+                </a>
+              </div>
+              <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a href="views/images/back2.JPG"  class="fancybox" rel="ligthbox">
+                  <img  src="views/images/back2.JPG" class="zoom img-fluid"  alt="">
+                </a>
+              </div>
+
+              <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a href="https://images.pexels.com/photos/158827/field-corn-air-frisch-158827.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="fancybox" rel="ligthbox">
+                  <img  src="https://images.pexels.com/photos/158827/field-corn-air-frisch-158827.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="zoom img-fluid "  alt="">
+                </a>
+              </div>
+      
+              <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a href="https://images.pexels.com/photos/302804/pexels-photo-302804.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="fancybox" rel="ligthbox">
+                  <img  src="https://images.pexels.com/photos/302804/pexels-photo-302804.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="zoom img-fluid "  alt="">
+                </a>
+              </div>
+      
+              <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a href="https://images.pexels.com/photos/1038914/pexels-photo-1038914.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="fancybox" rel="ligthbox">
+                  <img  src="https://images.pexels.com/photos/1038914/pexels-photo-1038914.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="zoom img-fluid "  alt="">
+                </a>
+              </div>
+      
+              <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a href="https://images.pexels.com/photos/414645/pexels-photo-414645.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="fancybox" rel="ligthbox">
+                  <img  src="https://images.pexels.com/photos/414645/pexels-photo-414645.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="zoom img-fluid "  alt="">
+                </a>
+              </div>
+      
+              <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a href="https://images.pexels.com/photos/56005/fiji-beach-sand-palm-trees-56005.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="fancybox" rel="ligthbox">
+                  <img  src="https://images.pexels.com/photos/56005/fiji-beach-sand-palm-trees-56005.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="zoom img-fluid "  alt="">
+                </a>
+              </div>
+      
+              <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a href="https://images.pexels.com/photos/1038002/pexels-photo-1038002.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="fancybox" rel="ligthbox">
+                  <img  src="https://images.pexels.com/photos/1038002/pexels-photo-1038002.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="zoom img-fluid "  alt="">
+                </a>
+              </div>
+            </div>
+          </div>
+  </article>
+</section>
---
+++ cuchillas/modules/navegacion.php
@@ -0,0 +1,23 @@
+<nav class="nav">
+  <div class="burger">
+    <div class="burger__patty"></div>
+  </div>
+
+  <ul class="nav__list">
+    <li class="nav__item">
+      <a href="#1" class="nav__link c-blue"><i class="fa fa fa-scissors"></i></a>
+    </li>
+    <li class="nav__item">
+      <a href="#2" class="nav__link c-blue scrolly"><i class="fa fa-users"></i></a>
+    </li>
+    <li class="nav__item">
+      <a href="#3" class="nav__link c-blue"><i class="fa fa-music"></i></a>
+    </li>
+    <li class="nav__item">
+      <a href="#4" class="nav__link c-blue"><i class="fa fa-play"></i></a>
+    </li>
+    <li class="nav__item">
+      <a href="#5" class="nav__link c-blue"><i class="fa fa-paper-plane"></i></a>
+    </li>
+  </ul>
+</nav>
